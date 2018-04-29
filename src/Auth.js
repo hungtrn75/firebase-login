1381d4b16351869c742161c215a2a0c27e985b52
hungtrn75 <hungtrn75@gmail.com>
2018-04-29 13:58:10 +0700

    Clean project

--- src/Auth.js
+++ src/Auth.js
@@ -12,11 +12,78 @@ var config = {
 firebase.initializeApp(config);
 
 class Auth extends Component {
+    logIn() {
+        let email = this.refs.email.value;
+        let password = this.refs.password.value;
+
+        firebase.auth().signInWithEmailAndPassword(email, password).then(user => {
+            var lin = document.getElementById('login');
+            var sup = document.getElementById('signup');
+            var lout = document.getElementById('logout');
+            lout.classList.remove('hide');
+            sup.classList.add('hide');
+            lin.classList.add('hide');
+            this.setState({
+                err: user.email
+            })
+        }).catch(error => {
+            var err = error.message;
+            this.setState({
+                err
+            });
+        });
+    }
+
+    signUp() {
+        let email = this.refs.email.value;
+        let password = this.refs.password.value;
+
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(user => {
+            var err = `Welcome ${user.email}`;
+            firebase.database().ref('users/' + user.uid).set({
+                email: user.email
+            });
+            this.setState({
+                err
+            });
+        }).catch((error) => {
+            var err = error.message;
+            this.setState({
+                err
+            });
+        });
+    }
+
+    logOut() {
+        firebase.auth().signOut();
+        var lin = document.getElementById('login');
+        var sup = document.getElementById('signup');
+        var lout = document.getElementById('logout');
+        lout.classList.add('hide');
+        sup.classList.remove('hide');
+        lin.classList.remove('hide');
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            err: ''
+        };
+        this.logIn = this.logIn.bind(this);
+        this.signUp = this.signUp.bind(this);
+        this.logOut = this.logOut.bind(this);
+    }
+
+
     render() {
         return (
             <div>
-                <input id="email" type="email" placeholder="example@example.com" /><br/>;
-                <input id="pass" type="password" placeholder="Your password" /><br/>
+                <input id="email" type="email" placeholder="example@example.com" ref="email" /><br />;
+                <input id="pass" type="password" placeholder="Your password" ref="password" /><br />
+                <p>{this.state.err}</p>
+                <button onClick={this.logIn} id="login">Login</button>
+                <button onClick={this.signUp} id="signup">Sign up</button>
+                <button onClick={this.logOut} id="logout" className="hide">Log out</button>
             </div>
         );
     }
